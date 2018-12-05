f7b21d36daeb2383baef4a7ba672d02a3c0af572
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-12-05 10:07:09 +0000

    feat: create #check_for_winner

--- lib/snakes_and_ladders.rb
+++ lib/snakes_and_ladders.rb
@@ -1,17 +1,30 @@
 class SnakesAndLadders 
 
-  attr_reader :token
+  attr_reader :token, :win_status
 
   def initialize
     @token = { "position" => 0 }
+    @win_status = false
   end
 
   def start
     @token["position"] = 1
   end
 
+  def roll_die
+    move(rand(1..6))
+  end
+
   def move(number)
     new_position = @token["position"] + number
     @token["position"] = new_position
+    check_for_winner
+    @token["position"]
+  end
+
+  def check_for_winner
+    if @token["position"] == 100
+      @win_status = true
+    end
   end
 end
--- spec/snakes_and_ladders_spec.rb
+++ spec/snakes_and_ladders_spec.rb
@@ -17,21 +17,55 @@ describe SnakesAndLadders do
     end
   end
 
-  context "when the token is on square 1" do
-    describe "then it's moved 3 spaces" do
-      it "will be on sqaure 4" do
-        game.start
-        expect(game.move(3)).to eq(4)
-        expect(game.token["position"]).to eq(4)
+  describe "#move" do
+    context "when the token is on square 1" do
+      describe "then it's moved 3 spaces" do
+        it "will be on square 4" do
+          game.start
+          expect(game.move(3)).to eq(4)
+          expect(game.token["position"]).to eq(4)
+        end
+
+        describe "then it's moved another 4 spaces" do
+          it "will be on square 8" do
+            game.start
+            game.move(3)
+            expect(game.move(4)).to eq(8)
+            expect(game.token["position"]).to eq(8)
+          end
+        end
+      end
+    end
+  end
+
+  describe "#roll_die" do
+    it "a random number between 1 and 6 is generated" do
+      allow(game).to receive(:rand).and_return(4)
+      expect(game.roll_die).to eq(4)
+    end
+
+    it "moves the token by rolled amount" do
+      game.start
+      allow(game).to receive(:rand).and_return(4)
+      expect(game.roll_die).to eq(5)
+      expect(game.token["position"]).to eq(5)
+    end
+  end
+
+  describe "#check_for_winner" do
+    context "when token position is 100" do
+
+      before(:each) do
+        game.token["position"] = 100
       end
 
-      describe "then it's moved another 4 spaces" do
-        it "will be on square 8" do
-          game.start
-          game.move(3)
-          expect(game.move(4)).to eq(8)
-          expect(game.token["position"]).to eq(8)
-        end
+      it "returns true" do
+        expect(game.check_for_winner).to eq(true)
+      end
+
+      it "sets win_status to true" do
+        game.check_for_winner
+        expect(game.win_status).to eq(true)
       end
     end
   end
