277058ddef60779cb12e11146e0869ad721c1881
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-12-05 08:58:59 +0000

    feat: the token's position will start at 0

--- lib/snakes_and_ladders.rb
+++ lib/snakes_and_ladders.rb
@@ -3,10 +3,10 @@ class SnakesAndLadders
   attr_reader :token
 
   def initialize
-    @token = { "position" => 1 }
+    @token = { "position" => 0 }
   end
 
   def start
-
+    @token["position"] = 1
   end
 end
--- spec/snakes_and_ladders_spec.rb
+++ spec/snakes_and_ladders_spec.rb
@@ -4,8 +4,14 @@ describe SnakesAndLadders do
 
   let(:game) { SnakesAndLadders.new }
 
+  context "before a game is started" do
+    it "the position will be 0" do
+      expect(game.token["position"]).to eq(0)
+    end
+  end
+
   context "when a game is started" do
-    it "will place the token on square 1" do
+    it "the token will be on square 1" do
       game.start
       expect(game.token["position"]).to eq(1)
     end
